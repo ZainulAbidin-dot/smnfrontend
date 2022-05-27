8133e128575ff5bd71cd4e2a0c387b1889a1fd0e
ZainulAbidin-dot <zchxvr@gmail.com>
2022-05-27 14:49:44 +0500

    first commit

--- src/components/routing/PrivateRoute.js
+++ src/components/routing/PrivateRoute.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-// import Spinner from "../layout/Spinner";
+// import React from "react";
+// import { Navigate } from "react-router-dom";
+// import PropTypes from "prop-types";
+// import { connect } from "react-redux";
+// // import Spinner from "../layout/Spinner";
 
-const PrivateRoute = ({
-  component: Component,
-  auth: { isAuthenticated, loading },
-}) => {
-  // if (loading) return <Spinner />;
-  if (isAuthenticated) return <Component />;
+// const PrivateRoute = ({
+//   component: Component,
+//   auth: { isAuthenticated, loading },
+// }) => {
+//   // if (loading) return <Spinner />;
+//   if (isAuthenticated) return <Component />;
 
-  return <Navigate to="/login" />;
-};
+//   return <Navigate to="/login" />;
+// };
 
-PrivateRoute.propTypes = {
-  auth: PropTypes.object.isRequired,
-};
+// PrivateRoute.propTypes = {
+//   auth: PropTypes.object.isRequired,
+// };
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-});
+// const mapStateToProps = (state) => ({
+//   auth: state.auth,
+// });
 
-export default connect(mapStateToProps)(PrivateRoute);
+// export default connect(mapStateToProps)(PrivateRoute);
