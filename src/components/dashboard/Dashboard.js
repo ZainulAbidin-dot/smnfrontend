c9e8ba68c7f6a7f520102bfc8fb46d9b7921bef9
ZainulAbidin-dot <zchxvr@gmail.com>
2022-05-27 22:13:27 +0500

    first commit

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -24,26 +24,28 @@ const Dashboard = ({
       <p className="lead">
         <i className="fas fa-user" /> Welcome User
       </p>
-      {profile !== null ? (
-        <>
-          <DashboardActions />
-          {/* <Experience experience={profile.experience} /> */}
-          {/* <Education education={profile.education} /> */}
+      {/* {profile !== null ? ( */}
+      <>
+        <DashboardActions />
+        {/* <Experience experience={profile.experience} /> */}
+        {/* <Education education={profile.education} /> */}
 
-          <div className="my-2">
-            <button className="btn btn-danger" onClick={() => deleteAccount()}>
-              <i className="fas fa-user-minus" /> Delete My Account
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <p>You have not yet setup a profile, please add some info</p>
-          <Link to="/create-profile" className="btn btn-primary my-1">
-            Create Profile
-          </Link>
-        </>
-      )}
+        <div className="my-2">
+          <button className="btn btn-danger" onClick={() => deleteAccount()}>
+            <i className="fas fa-user-minus" /> Delete My Account
+          </button>
+        </div>
+      </>
+      {/* // ) : ( //{" "}
+      <>
+        // <p>You have not yet setup a profile, please add some info</p>
+        //{" "}
+        <Link to="/create-profile" className="btn btn-primary my-1">
+          // Create Profile //{" "}
+        </Link>
+        //{" "}
+      </>
+      // )} */}
     </section>
   );
 };
