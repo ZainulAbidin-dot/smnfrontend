5774eaf9372f5e184e707ba45d316167977f099b
ZainulAbidin-dot <zchxvr@gmail.com>
2022-05-27 22:18:02 +0500

    first commit

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,12 +8,7 @@ import DashboardActions from "./DashboardActions";
 import Experience from "./Experience";
 import Education from "./Education";
 
-const Dashboard = ({
-  getCurrentProfile,
-  deleteAccount,
-  auth: { user },
-  profile: { profile, loading },
-}) => {
+const Dashboard = () => {
   // useEffect(() => {
   //   getCurrentProfile();
   // }, [getCurrentProfile]);
@@ -31,7 +26,7 @@ const Dashboard = ({
         {/* <Education education={profile.education} /> */}
 
         <div className="my-2">
-          <button className="btn btn-danger" onClick={() => deleteAccount()}>
+          <button className="btn btn-danger">
             <i className="fas fa-user-minus" /> Delete My Account
           </button>
         </div>
@@ -48,13 +43,6 @@ const Dashboard = ({
       // )} */}
     </section>
   );
-};
-
-Dashboard.propTypes = {
-  getCurrentProfile: PropTypes.func.isRequired,
-  deleteAccount: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
